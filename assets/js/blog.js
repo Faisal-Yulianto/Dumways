1ee80116d81c7402dcfe2ffbf313713d5531b3a5
faisal yulianto <faisalyulianto26@gmail.com>
2024-08-03 15:08:35 +0700

    routing day11

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,8 +1,3 @@
-const btn = document.getElementById("hamIcon");
-const navHam = document.getElementById("hamList")
-btn.addEventListener("click",function () {
-    navHam.classList.toggle("show")
-})
 
 let dataBlog = [];
 function addBlog(event){
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const path = require('path'); 
 const app = express()
 const port = 3000
 
 app.set("view engine","hbs");
-app.set("views","assets/views");
+app.set("views", path.join(__dirname, 'views'));
+app.use("/assets", express.static("assets"));
 
 app.get('/', (req, res) => {
   res.render('index');
-})
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/blog', (req, res) => {
+  res.render('blog');
+});
+
+app.get('/testi', (req, res) => {
+  res.render('testi');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
